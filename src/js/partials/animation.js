176242bf9306338c6614c0cdcd0e445e7f0b2cf8
Roman <Roman@Romans-MacBook-Pro.local>
2017-09-03 22:03:58 +0300

    remove figures from article and add animation for request form

--- src/js/partials/animation.js
+++ src/js/partials/animation.js
@@ -1,29 +1,28 @@
-$(function() {
-	if(window.innerWidth>=768){
-    var scrollMagicController = new ScrollMagic();
+if(window.innerWidth>=768){
+  var scrollMagicController = new ScrollMagic();
 
-    $('.posts .post').each(function(){
+  jQuery('.posts .post').each(function(){
 
-    var currentArticle = $(this);
-    var post_animation = TweenMax.staggerFromTo(currentArticle, 0.5, {
-        position: "relative",
-        opacity: 0,
-        bottom: "-70px"
-      },
-      {
-        position: "relative",
-        opacity: 1,
-        bottom: "0"
-      },
-      0.2);
+  var currentArticle = $(this);
+  var post_animation = TweenMax.staggerFromTo(currentArticle, 0.5, {
+      position: "relative",
+      opacity: 0,
+      bottom: "-70px"
+    },
+    {
+      position: "relative",
+      opacity: 1,
+      bottom: "0"
+    },
+    0.2);
 
-    var posts = new ScrollScene({
-      triggerElement: currentArticle,
-      triggerHook: "onEnter"
+  var posts = new ScrollScene({
+    triggerElement: currentArticle,
+    triggerHook: "onEnter"
 
-    })
-      .setTween(post_animation)
-      .addTo(scrollMagicController)
-    });
-  }
-})();
+  })
+    .setTween(post_animation)
+    .addTo(scrollMagicController)
+  });
+};
+
--- src/js/partials/scripts.js
+++ src/js/partials/scripts.js
@@ -1,38 +1,55 @@
 (function () {
-
-	$('#js-mobile-nav-btn').click(function(){
-		$('body').addClass('overflow-hidden');
-		$('#js-mobile-nav').addClass('open');
-	});
-	$('#js-mobile-nav-close-btn').click(function(){
-		$('body').removeClass('overflow-hidden');
-		$('#js-mobile-nav').removeClass('open');
-	});
-
-  $('.js-form-btn').click(function(){
-    if($('#js-mobile-nav').hasClass('open')){
-      $('#js-mobile-nav').removeClass('open');
+  jQuery('.article h3').each(function(i,elem) {
+    var titleNumber = i + 1;
+    var counterValue;
+    if (titleNumber < 10){
+      counterValue = "0" + titleNumber
+    }else{
+      counterValue = titleNumber
     }
-    $('#js-form').addClass('open');
-    if($('body').hasClass('overflow-hidden')){
+    jQuery(this).append( "<a class='article__counter'></a>" );
+    jQuery(this).attr( 'id', 'header-' + titleNumber);
+    jQuery(this).children(".article__counter").attr( 'href', '#header-' + titleNumber);
+    jQuery(this).children(".article__counter").text(counterValue);
+
+
+  });
+  jQuery('#js-mobile-nav-btn').click(function(){
+    jQuery('body').addClass('overflow-hidden');
+    jQuery('#js-mobile-nav').addClass('open');
+	});
+  jQuery('#js-mobile-nav-close-btn').click(function(){
+    jQuery('body').removeClass('overflow-hidden');
+    jQuery('#js-mobile-nav').removeClass('open');
+	});
+
+  jQuery('.js-form-btn').click(function(){
+    if( jQuery('#js-mobile-nav').hasClass('open')){
+      jQuery('#js-mobile-nav').removeClass('open');
+    }
+    jQuery('#js-form').addClass('open');
+    if(jQuery('body').hasClass('overflow-hidden')){
       return;
     }else{
-      $('body').addClass('overflow-hidden');
+      jQuery('body').addClass('overflow-hidden');
     }
 
   });
-  $('.js-form-close-btn').click(function(){
-    $('body').removeClass('overflow-hidden');
-    $('#js-form').removeClass('open');
+  jQuery('.js-form-close-btn').click(function(){
+    jQuery('body').removeClass('overflow-hidden');
+    jQuery('#js-form').removeClass('open');
   });
 
 
-	$('a[href*="#"]').bind("click", function(e){
+  jQuery('a[href*="#"]').bind("click", function(e){
 		var anchor = $(this);
-		$('html, body').stop().animate({
+    jQuery('html, body').stop().animate({
 			scrollTop: $(anchor.attr('href')).offset().top}, 1000);
 		e.preventDefault();
 	});
+
+
+
 
 }());
 
