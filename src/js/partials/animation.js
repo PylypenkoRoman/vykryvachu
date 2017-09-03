73d48cc7b08debc86ae263158db532f0133f282c
Roman <Roman@Romans-MacBook-Pro.local>
2017-09-03 13:59:47 +0300

    v 4.0

--- src/js/partials/animation.js
+++ src/js/partials/animation.js
@@ -1,30 +1,29 @@
 $(function() {
+	if(window.innerWidth>=768){
+    var scrollMagicController = new ScrollMagic();
 
-	var scrollMagicController = new ScrollMagic();
+    $('.posts .post').each(function(){
 
-	$('.posts .post').each(function(){
+    var currentArticle = $(this);
+    var post_animation = TweenMax.staggerFromTo(currentArticle, 0.5, {
+        position: "relative",
+        opacity: 0,
+        bottom: "-70px"
+      },
+      {
+        position: "relative",
+        opacity: 1,
+        bottom: "0"
+      },
+      0.2);
 
-	var currentArticle = $(this);
-	var post_animation = TweenMax.staggerFromTo(currentArticle, 0.5, {
-			position: "relative",
-			opacity: 0,
-			bottom: "-70px"
-		},
-		{
-			position: "relative",
-			opacity: 1,
-			bottom: "0"
-		},
-		0.2);
+    var posts = new ScrollScene({
+      triggerElement: currentArticle,
+      triggerHook: "onEnter"
 
-	var posts = new ScrollScene({
-		triggerElement: currentArticle,
-		triggerHook: "onEnter"
-
-	})
-		.setTween(post_animation)
-		.addTo(scrollMagicController)
-		.addIndicators();
-	});
-
+    })
+      .setTween(post_animation)
+      .addTo(scrollMagicController)
+    });
+  }
 })();
